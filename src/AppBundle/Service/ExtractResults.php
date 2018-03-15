f64e94409ee0dfb6dfe2175be4186c328d8ad7dc
sjacquot <stephane.jacquot@gmail.com>
2018-03-15 19:23:18 +0100

    Filter extraction of result with row visibility

--- src/AppBundle/Service/ExtractResults.php
+++ src/AppBundle/Service/ExtractResults.php
@@ -46,138 +46,140 @@ class ExtractResults
         $resultsArray = array();
 
         for ($row = 1; $row <= $highestRow; $row++){
-            $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row)->getCalculatedValue();
-            // read $value until remarkable value found to indicate start of a table
-            if(strpos($value, self::BAI) !== false){ // STR COMP With Type
-                // go to the start of Values to be extracted
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
-                    }
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::BAI][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
-                }
-            }
-            if(strpos($value, self::BAE) !== false){ // STR COMP With Types
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
-                    }
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::BAE][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
-                }
-            }
-            if(strpos($value, self::BC) !== false){ // STR COMP With Types
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
-                    }
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::BC][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
-                }
-            }
-            if(strpos($value, self::BEVMC) !== false){ // STR COMP With Types
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
-                        if($Celldata !== null){
-                            $data[] = $Celldata;
+            if ($worksheet->getRowDimension($row)->getVisible()){
+                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row)->getCalculatedValue();
+                // read $value until remarkable value found to indicate start of a table
+                if(strpos($value, self::BAI) !== false){ // STR COMP With Type
+                    // go to the start of Values to be extracted
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
                         }
-                    }
-                    $Celldata = $worksheet->getCellByColumnAndRow(4, ++$row)->getCalculatedValue();
-                    if($Celldata !== null){
-                        $data[1] = $data[1].self::WORDLINEBR.$Celldata;
-                    }
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::BEVMC][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
-                }
-            }
-            if(strpos($value, self::BEIEL) !== false){ // STR COMP With Types
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
-                        if($Celldata !== null){
-                            $data[] = $Celldata;
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::BAI][] = $data;
                         }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
                     }
-                    $Celldata = $worksheet->getCellByColumnAndRow(4 /* col D */, ++$row)->getCalculatedValue();
-                    if($Celldata !== null){
-                        $data[1] = $data[1].self::WORDLINEBR.$Celldata;
-                    }
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::BEIEL][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
                 }
-            }
-            if(strpos($value, self::BEIIL) !== false){ // STR COMP With Types
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
-                        if($Celldata !== null){
-                            $data[] = $Celldata;
+                if(strpos($value, self::BAE) !== false){ // STR COMP With Types
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
                         }
-                    }
-                    $Celldata = $worksheet->getCellByColumnAndRow(4 /* col D */, ++$row)->getCalculatedValue();
-                    if($Celldata !== null){
-                        $data[1] = $data[1].self::WORDLINEBR.$Celldata;
-                    }
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::BEIIL][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
-                }
-            }
-            if(strpos($value, self::BEC) !== false){ // STR COMP With Types
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
-                        if($Celldata !== null){
-                            $data[] = $Celldata;
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::BAE][] = $data;
                         }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
                     }
-                    $data[1] = $data[1].'<br>'.$worksheet->getCellByColumnAndRow(4 /* col D */, ++$row)->getCalculatedValue();
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::BEC][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
                 }
-            }
-            if(strpos($value, self::AAE) !== false){ // STR COMP With Types
-                $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 4)->getCalculatedValue();
-                while(strlen($value)>= 1){
-                    $data = array();
-                    foreach ($dataInCols as $col ){
-                        $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+                if(strpos($value, self::BC) !== false){ // STR COMP With Types
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+                        }
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::BC][] = $data;
+                        }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
                     }
-                    if(strlen($data[0])>0){
-                        $resultsArray[self::AAE][] = $data;
-                    }
-                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
                 }
+                if(strpos($value, self::BEVMC) !== false){ // STR COMP With Types
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+                            if($Celldata !== null){
+                                $data[] = $Celldata;
+                            }
+                        }
+                        $Celldata = $worksheet->getCellByColumnAndRow(4, ++$row)->getCalculatedValue();
+                        if($Celldata !== null){
+                            $data[1] = $data[1].self::WORDLINEBR.$Celldata;
+                        }
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::BEVMC][] = $data;
+                        }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
+                    }
+                }
+                if(strpos($value, self::BEIEL) !== false){ // STR COMP With Types
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+                            if($Celldata !== null){
+                                $data[] = $Celldata;
+                            }
+                        }
+                        $Celldata = $worksheet->getCellByColumnAndRow(4 /* col D */, ++$row)->getCalculatedValue();
+                        if($Celldata !== null){
+                            $data[1] = $data[1].self::WORDLINEBR.$Celldata;
+                        }
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::BEIEL][] = $data;
+                        }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
+                    }
+                }
+                if(strpos($value, self::BEIIL) !== false){ // STR COMP With Types
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+                            if($Celldata !== null){
+                                $data[] = $Celldata;
+                            }
+                        }
+                        $Celldata = $worksheet->getCellByColumnAndRow(4 /* col D */, ++$row)->getCalculatedValue();
+                        if($Celldata !== null){
+                            $data[1] = $data[1].self::WORDLINEBR.$Celldata;
+                        }
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::BEIIL][] = $data;
+                        }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
+                    }
+                }
+                if(strpos($value, self::BEC) !== false){ // STR COMP With Types
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 7)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $Celldata = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+                            if($Celldata !== null){
+                                $data[] = $Celldata;
+                            }
+                        }
+                        $data[1] = $data[1].'<br>'.$worksheet->getCellByColumnAndRow(4 /* col D */, ++$row)->getCalculatedValue();
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::BEC][] = $data;
+                        }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, ++$row)->getCalculatedValue();
+                    }
+                }
+                if(strpos($value, self::AAE) !== false){ // STR COMP With Types
+                    $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row += 4)->getCalculatedValue();
+                    while(strlen($value)>= 1){
+                        $data = array();
+                        foreach ($dataInCols as $col ){
+                            $data[] = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+                        }
+                        if(strlen($data[0])>0){
+                            $resultsArray[self::AAE][] = $data;
+                        }
+                        $value = $worksheet->getCellByColumnAndRow(2 /* col B */, $row++)->getCalculatedValue();
+                    }
 
+                }
             }
         }
 
